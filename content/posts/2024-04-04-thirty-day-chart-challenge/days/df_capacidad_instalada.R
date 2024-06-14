252c038afd9c8b274bdcc10485f6f28385a70c56
pablotis <pablotisco@gmail.com>
2024-06-14 11:51:47 -0300

    actualizo grafico cap_instalada

--- content/posts/2024-04-04-thirty-day-chart-challenge/days/df_capacidad_instalada.R
+++ content/posts/2024-04-04-thirty-day-chart-challenge/days/df_capacidad_instalada.R
@@ -1,5 +1,4 @@
 
-url <- 
 df_capacidad_inst_orig <- readxl::read_excel(here::here("content/posts/2024-04-04-thirty-day-chart-challenge/days/sh_capacidad_06_24.xls"), 
                                              skip = 2)
 
